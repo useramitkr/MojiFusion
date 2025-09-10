d26b340717a3ce7e517ce0224767d1b625df3b7a
Amit Kumar <useramit2016@gmail.com>
2025-09-10 17:35:16 +0530

    Admob Inline Ad

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,18 @@
 import { GameProvider } from "@/context/GameContext";
 import { Stack } from "expo-router";
+import { useEffect } from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import mobileAds from 'react-native-google-mobile-ads';
 
 export default function RootLayout() {
+
+  // Initialize Google Mobile Ads SDK
+  // useEffect(() => {
+  //   (async () => {
+  //     await mobileAds().initialize();
+  //   })();
+  // }, [])
+
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <GameProvider>
---
+++ components/AdmobAd/InlineAd.tsx
@@ -0,0 +1,32 @@
+// InlineAd.tsx
+
+import { View } from 'react-native';
+import * as Device from 'expo-device';
+import React, { useState } from 'react';
+import { BannerAd, BannerAdSize, TestIds } from 'react-native-google-mobile-ads';
+
+const iosAdmobBanner = "ca-app-pub-12345678910/12345678910";
+const androidAdmobBanner = "ca-app-pub-12345678910/12345678910";
+const productionID = Device.osName === 'Android' ? androidAdmobBanner : iosAdmobBanner;
+
+const InlineAd = () => {
+  const [isAdLoaded, setIsAdLoaded] = useState<boolean>(false);
+  return (
+    <View style={{ height: isAdLoaded ? 'auto' : 0 }}>
+      <BannerAd
+        // It is extremely important to use test IDs as you can be banned/restricted by Google AdMob for inappropriately using real ad banners during testing
+        unitId={__DEV__ ? TestIds.BANNER : productionID}
+        size={BannerAdSize.ANCHORED_ADAPTIVE_BANNER}
+        requestOptions={{
+          requestNonPersonalizedAdsOnly: true, 
+          // You can change this setting depending on whether you want to use the permissions tracking we set up in the initializing
+        }}
+        onAdLoaded={() => {
+          setIsAdLoaded(true);
+        }}
+      />
+    </View >
+  );
+};
+
+export default InlineAd;
